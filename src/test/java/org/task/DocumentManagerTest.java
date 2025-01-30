25492f8c2311a32ed32245175644bc3bb7747a4d
Peter Popazov <popazovpeta@gmail.com>
2025-01-30 16:17:16 +0200

    init task

---
+++ src/test/java/org/task/DocumentManagerTest.java
@@ -0,0 +1,12 @@
+package org.task;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class DocumentManagerTest {
+
+    @Test
+    public void test_init() {
+        Assertions.assertTrue(true);
+    }
+}
